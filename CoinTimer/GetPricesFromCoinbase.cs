eda9b7a3948ff1893b373e1f8987abfad45215e8
pingzing <mcalistern@gmail.com>
2017-12-21 10:19:45 +0200

    It works now!

--- CoinTimer/GetPricesFromCoinbase.cs
+++ CoinTimer/GetPricesFromCoinbase.cs
@@ -1,114 +1,45 @@
+using Microsoft.Azure.NotificationHubs;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Host;
+using Microsoft.Toolkit.Uwp.Notifications;
+using Newtonsoft.Json;
 using System;
+using System.Collections.Generic;
 using System.Linq;
 using System.Net.Http;
 using System.Threading;
 using System.Threading.Tasks;
-using Microsoft.Azure.NotificationHubs;
-using Microsoft.Azure.WebJobs;
-using Microsoft.Azure.WebJobs.Extensions.NotificationHubs;
-using Microsoft.Azure.WebJobs.Host;
-using Microsoft.Toolkit.Uwp.Notifications;
-using Newtonsoft.Json;
 
 namespace CoinTimer
-{    
+{
     public static class GetPricesFromCoinbase
-    {        
+    {
         [FunctionName("GetPricesFromCoinbase")]
-        public static async Task Run([TimerTrigger("0 */5 * * * *")]TimerInfo myTimer, 
-                                     [NotificationHub]IAsyncCollector<Notification> notification,
-                                     TraceWriter log)
-        {            
+        public static async Task Run([TimerTrigger("0 */5 * * * *")]TimerInfo myTimer,
+            [NotificationHub(ConnectionStringSetting = "AzureWebJobsNotificationHubsConnectionString",
+                HubName = "cointesthub",
+                Platform = NotificationPlatform.Wns)] IAsyncCollector<Notification> notification,
+            TraceWriter log)
+        {
+            log.Info("Running!");
             CancellationTokenSource cts = new CancellationTokenSource(10_000);
 
             log.Info($"C# Timer trigger function executed at: {DateTime.Now}");
-            
+
             using (var httpClient = new HttpClient())
             {
                 try
                 {
                     httpClient.DefaultRequestHeaders.Add("CB-VERSION", "2017-08-07");
 
-                    HttpResponseMessage result = await httpClient.GetAsync($"https://api.coinbase.com/v2/prices/BTC-USC/spot", cts.Token);
-                    HttpResponseMessage ltcResult = await httpClient.GetAsync($"https://api.coinbase.com/v2/prices/LTC-USD/spot", cts.Token);
+                    List<TileContent> tiles = new List<TileContent>();
+                    tiles.Add(await GetCoinTileContent(httpClient, "BTC", cts.Token, log));
+                    tiles.Add(await GetCoinTileContent(httpClient, "LTC", cts.Token, log));
+                    tiles.Add(await GetCoinTileContent(httpClient, "BCH", cts.Token, log));
 
-                    if (result.IsSuccessStatusCode && ltcResult.IsSuccessStatusCode)
+                    foreach (var successfulTile in tiles.Where(x => x != null))
                     {
-                        string btcString = await result.Content.ReadAsStringAsync();
-                        CoinbaseResponse btcResponse = JsonConvert.DeserializeObject<CoinbaseResponse>(btcString);
-
-                        string ltcString = await ltcResult.Content.ReadAsStringAsync();
-                        CoinbaseResponse ltcResponse = JsonConvert.DeserializeObject<CoinbaseResponse>(ltcString);
-
-                        TileVisual tileVisual = new TileVisual()
-                        {
-                            TileSmall = new TileBinding
-                            {
-                                Content = new TileBindingContentAdaptive
-                                {
-                                    TextStacking = TileTextStacking.Center,
-                                    Children = 
-                                    {
-                                        new AdaptiveText { Text = "BTC to USD" },
-                                        new AdaptiveText { Text = $"${btcResponse.Data.Amount}", HintStyle = AdaptiveTextStyle.CaptionSubtle }
-                                    }
-                                }
-                            },
-
-                            TileMedium = new TileBinding
-                            {
-                                Content = new TileBindingContentAdaptive
-                                {
-                                    TextStacking = TileTextStacking.Center,
-                                    Children =
-                                    {
-                                        new AdaptiveText { Text = "BTC to USD" },
-                                        new AdaptiveText { Text = $"${btcResponse.Data.Amount}", HintStyle = AdaptiveTextStyle.CaptionSubtle }
-                                    }
-                                }
-                            },
-
-                            TileLarge = new TileBinding
-                            {
-                                Content = new TileBindingContentAdaptive
-                                {
-                                    TextStacking = TileTextStacking.Center,
-                                    Children =
-                                    {
-                                        new AdaptiveText { Text = "BTC to USD" },
-                                        new AdaptiveText { Text = $"${btcResponse.Data.Amount}", HintStyle = AdaptiveTextStyle.CaptionSubtle },
-                                        new AdaptiveText { Text = "LTC to USD"},
-                                        new AdaptiveText { Text = $"${ltcResponse.Data.Amount}", HintStyle = AdaptiveTextStyle.CaptionSubtle}
-                                    }
-                                }
-                            },
-
-                            TileWide = new TileBinding
-                            {
-                                Content = new TileBindingContentAdaptive
-                                {
-                                    TextStacking = TileTextStacking.Center,
-                                    Children =
-                                    {
-                                        new AdaptiveText { Text = "BTC to USD" },
-                                        new AdaptiveText { Text = $"${btcResponse.Data.Currency}", HintStyle = AdaptiveTextStyle.CaptionSubtle },
-                                        new AdaptiveText { Text = "LTC to USD"},
-                                        new AdaptiveText { Text = $"${ltcResponse.Data.Amount}", HintStyle = AdaptiveTextStyle.CaptionSubtle}
-                                    }
-                                }
-                            }
-                        };
-
-                        TileContent tileContent = new TileContent
-                        {
-                            Visual = tileVisual
-                        };
-
-                        await notification.AddAsync(new WindowsNotification(tileContent.GetContent()));
-                    }
-                    else
-                    {
-                        log.Error($"Failed to get a response from Coinbase. HTTP Status: {result.StatusCode}, Response body: {await result.Content.ReadAsStringAsync()}");
+                        await notification.AddAsync(new WindowsNotification(successfulTile.GetContent()));
                     }
                 }
                 catch (Exception ex) when (ex is OperationCanceledException || ex is HttpRequestException)
@@ -120,7 +51,85 @@ namespace CoinTimer
                     log.Error($"Failed to Coinbase info. Exception: {ex.Message}.");
                 }
             }
-
         }
+
+        private static async Task<TileContent> GetCoinTileContent(HttpClient httpClient, string coinType, CancellationToken token, TraceWriter log)
+        {
+            HttpResponseMessage result = await httpClient.GetAsync($"https://api.coinbase.com/v2/prices/{coinType}-USD/spot", token);
+
+            if (result.IsSuccessStatusCode)
+            {
+                string coinString = await result.Content.ReadAsStringAsync();
+                CoinbaseResponse coinResponse = JsonConvert.DeserializeObject<CoinbaseResponse>(coinString);
+
+                TileVisual tileVisual = new TileVisual()
+                {
+                    TileSmall = new TileBinding
+                    {
+                        Content = new TileBindingContentAdaptive
+                        {
+                            TextStacking = TileTextStacking.Center,
+                            Children =
+                            {
+                                new AdaptiveText { Text = $"{coinType} to USD" },
+                                new AdaptiveText { Text = $"${coinResponse.Data.Amount}", HintStyle = AdaptiveTextStyle.CaptionSubtle }
+                            }
+                        }
+                    },
+
+                    TileMedium = new TileBinding
+                    {
+                        Content = new TileBindingContentAdaptive
+                        {
+                            TextStacking = TileTextStacking.Center,
+                            Children =
+                            {
+                                new AdaptiveText { Text = $"{coinType} to USD" },
+                                new AdaptiveText { Text = $"${coinResponse.Data.Amount}", HintStyle = AdaptiveTextStyle.CaptionSubtle }
+                            }
+                        }
+                    },
+
+                    TileLarge = new TileBinding
+                    {
+                        Content = new TileBindingContentAdaptive
+                        {
+                            TextStacking = TileTextStacking.Center,
+                            Children =
+                            {
+                                new AdaptiveText { Text = $"{coinType} to USD" },
+                                new AdaptiveText { Text = $"${coinResponse.Data.Amount}", HintStyle = AdaptiveTextStyle.CaptionSubtle },
+                            }
+                        }
+                    },
+
+                    TileWide = new TileBinding
+                    {
+                        Content = new TileBindingContentAdaptive
+                        {
+                            TextStacking = TileTextStacking.Center,
+                            Children =
+                            {
+                                new AdaptiveText { Text = $"{coinType} to USD" },
+                                new AdaptiveText { Text = $"${coinResponse.Data.Amount}", HintStyle = AdaptiveTextStyle.CaptionSubtle },
+                            }
+                        }
+                    }
+                };
+
+                TileContent tileContent = new TileContent
+                {
+                    Visual = tileVisual
+                };                
+
+                return tileContent;
+            }
+            else
+            {
+                log.Error($"Failed to get a response from Coinbase. HTTP Status: {result.StatusCode}, Response body: {await result.Content.ReadAsStringAsync()}");
+                return null;
+            }
+        }
+
     }
 }
